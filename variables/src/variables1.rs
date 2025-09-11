e736039a6c58e40fc39e520a3b786ac78fa5dbe8
anandteertha <raoanandteertha@gmail.com>
2025-09-11 16:44:31 -0400

    variables - part 1 add comments for better understanding

--- variables/src/variables1.rs
+++ variables/src/variables1.rs
@@ -1,7 +1,10 @@
 pub fn variable1() {
+    // number is an int.
     let number: i8 = 10;
     println!("number is {}", number);
+    // if we try to convert number directly to string it won't allow us.
     // number = "10";
+    // number is now a string..
     let number: &str = "11";
     println!("number is {}", number);
 }
