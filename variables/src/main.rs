9c391329a0b88a4d6d538568cb140dd969273104
anandteertha <raoanandteertha@gmail.com>
2025-09-12 15:01:22 -0400

    panicking example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,12 @@
 mod variables1;
 mod variables2;
 mod shadowing;
+mod panicking;
 
 fn main() {
     variables1::variable1();
     variables2::variable2();
     shadowing::not_shadow();
     shadowing::shadow();
+    panicking::panickkk();
 }
---
+++ variables/src/panicking.rs
@@ -0,0 +1,8 @@
+pub fn panickkk() {
+    println!("Panicking example..");
+    let arr: [u8; 3] = [0,1,2];
+    println!("arr value at index 0 is {}", arr[0]);
+    println!("arr value at index 1 is {}", arr[1]);
+    println!("arr value at index 2 is {}", arr[2]);
+    // println!("arr value at index 3 is {}", arr[3]); if you uncomment this and run.. the code will panick!
+}
